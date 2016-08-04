f22661039bd83c4a4d0e24ae305d643c98ab98ee
ranissan <ranissan@gmail.com>
2016-08-04 09:07:57 -0300

    Criar caixa de dialogo para passar parametros

--- Saudacao.java
+++ Saudacao.java
@@ -1,7 +1,28 @@
+import javax.swing.JOptionPane;
+import java.util.Scanner;
 class Saudacao {
+	
 	public static void main (String args[]){
+        
+        //System.out.print("Digite o nome do Aluno: ");
+        
+        // Declarando e instanciando o objeto entrada do tipo Scanner 
+        Scanner entrada = new Scanner(System.in);
 
-		System.out.println(args[0] + " Benvindo ao curso de Java");
+		/*
+         Aqui criamos o objeto que receberá o nome do aluno
+		
+		String nome = entrada.nextLine();
+         */
+        String nome = JOptionPane.showInputDialog("Digite o nome do Aluno: ");
+
+		System.out.println(nome + " Benvindo ao curso de Java");
      
+     // declara variaveis 
+        int idade;
+	 // Atribuicao da variavel
+	    idade = 33;
+	 // Exibição 
+        System.out.println("A idade de "+ nome + " é "+idade);
 	}
 }
